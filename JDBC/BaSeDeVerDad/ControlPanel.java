a3377325f801bcb94d57eb2cea1e7f22632dfbb2
elCreacionista <58265463+elCreacionista@users.noreply.github.com>
2021-05-12 00:46:51 +0200

    Actionlistener y botones

--- JDBC/BaSeDeVerDad/ControlPanel.java
+++ JDBC/BaSeDeVerDad/ControlPanel.java
@@ -3,25 +3,33 @@ package BaSeDeVerDad;
 import javax.print.attribute.AttributeSetUtilities;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class ControlPanel extends Container {
-    ControlPanel(int rows, int cols){
+    ControlPanel(){
         SetUp();
-        setLayout(new GridLayout(rows, cols));
     }
     private void SetUp(){
         setVisible(true);
-        setBackground(Color.lightGray);
+        setBackground(Color.red);
+        this.setLayout(1, 2);
+        AddButton("ola", new Color(11111), new ButtonFunction("ola"));
+        AddButton("ola2", Color.gray, new ButtonFunction("ola2"));
     }
     public void setLayout(int rows, int cols){
-        setLayout(new GridLayout(rows, cols));
+        setLayout(new GridLayout(rows, cols, 50, 50));
     }
 
-    public void AddButton(String text, Color color){
+    public void AddButton(String text, Color color, ActionListener al ){
         JButton button = new JButton(text);
         button.setBackground(color);
         button.setVisible(true);
+        button.addActionListener(al);
         add(button);
+        TestLayout();
+    }
+    private void TestLayout(){
+        System.out.println(this.getComponents().length);
     }
 
 }
