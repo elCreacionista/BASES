c375080be1ef7cd6a045cda0a17c2ee51766f6df
elCreacionista <58265463+elCreacionista@users.noreply.github.com>
2021-05-28 20:20:01 +0200

    cosas

--- JDBC/BaSeDeVerDad/Metadatas/Card.java
+++ JDBC/BaSeDeVerDad/Metadatas/Card.java
@@ -6,6 +6,18 @@ public class Card {
     Date expiration;
     Type type;
 
+    Card(int id, int number, Date date_expiration, String type){
+        this.number = number;
+        this.id = id;
+        this.expiration = date_expiration;
+        switch (type.toLowerCase()) {
+            case "visa" -> this.type = Type.Visa;
+            case "mastercard" -> this.type = Type.Mastercard;
+            case "maestro" -> this.type = Type.Maestro;
+            default -> this.type = Type.Visa;
+        }
+    }
+
     enum Type{
         Visa,Mastercard,Maestro
     }
